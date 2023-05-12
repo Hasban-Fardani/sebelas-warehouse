633e96f7f71220a722c81a9610c2ddbb94ffd66d
SammLl <samueltel396@gmail.com>
2023-05-12 10:39:27 +0700

    create login form

--- sebelas-warehouse/LoginForm.cs
+++ sebelas-warehouse/LoginForm.cs
@@ -16,5 +16,45 @@ namespace sebelas_warehouse
         {
             InitializeComponent();
         }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
